72fe1983cc51c647d00bf45cde64b0b979905ad2
Jhabriel Varela <jhabriel@gmail.com>
2019-06-18 18:01:06 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# hydrogen-cell-separator
---
+++ spec/hydrogen-cell-separator-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import HydrogenCellSeparatorView from '../lib/hydrogen-cell-separator-view';
+
+describe('HydrogenCellSeparatorView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
