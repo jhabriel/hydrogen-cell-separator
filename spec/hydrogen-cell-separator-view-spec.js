3d82a4198ab367035392e9cc396aaec62eedb38d
Jhabriel Varela <jhabriel@gmail.com>
2019-06-18 18:26:17 +0200

    Remove unnecesary files

--- spec/hydrogen-cell-separator-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import HydrogenCellSeparatorView from '../lib/hydrogen-cell-separator-view';
-
-describe('HydrogenCellSeparatorView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
